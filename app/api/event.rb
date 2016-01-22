ae0638c220f7b7278b76773e5bcec476ec890feb
awwa500@gmail.com <awwa500@gmail.com>
2016-01-22 11:40:30 +0900

    update grape code

--- app/api/event.rb
+++ app/api/event.rb
@@ -1,16 +1,36 @@
 module Event
   class API < Grape::API
+
+    format :json
+
     helpers do
       def logger
         API.logger
       end
     end
+
     desc 'Receive Event Webhook'
-    params do
-      requires :events, type: JSON, desc: 'event list'
-    end
+    # params do
+    #   requires :json, type: Array[JSON] do
+    #     requires :event, type: String
+    #   end
+    # end
+    # params do
+    #   optional :json, type: Array do
+    #     requires :event, type: String
+    #   end
+    # end
     post do
-      logger.info "post event: #{params[:events].inspect}"
+      # Rails.logger = Logger.new(STDOUT)
+      #params[:json].each {|event| logger.info event.inspect}
+      logger.info params.to_s
+      p params
+      logger.info params.inspect
+      logger.info params.instance_variables
+      params.each do |event|
+        logger.info event#{}"post event: #{params[:json].inspect}"
+      end
+      status 201
     end
   end
 end
